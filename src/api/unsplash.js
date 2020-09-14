fb91ad2f1490c0aab7cd7f011b51b715493c581e
Aayush <mahapatraaayush@gmail.com>
2020-09-14 12:54:31 +0530

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: 'Client-ID hEDs_ZgN_ETZ06S2Il1Zd5dbDW9wLkqweMeiuiEa5cE'
+    Authorization: ''
   }
 });
